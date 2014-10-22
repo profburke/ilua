dd0ffa74a86bc1fb59e16e0cc07841c543b303f7
Matthew M. Burke <matthew@logicnets.com>
2014-10-22 13:00:07 -0400

    discontinuous change: created an xcode 6 project

---
+++ LuaInterp/LuaInterp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LuaInterp
+//
+//  Created by Matthew Burke on 10/22/14.
+//  Copyright (c) 2014 BlueDino Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
