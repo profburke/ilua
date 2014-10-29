4f4c673806dcd09818e5a86950087b8488483f3f
Matthew M. Burke <matthew@logicnets.com>
2014-10-29 15:36:14 -0400

    added files for custom library

---
+++ LuaInterp/bdlib.c
@@ -0,0 +1,42 @@
+//
+//  flexiprint.c
+//  LuaInterp
+//
+//  Created by Matthew Burke on 10/26/14.
+//  Copyright (c) 2014 BlueDino Software. All rights reserved.
+//
+
+#include "flexiprint.h"
+#include "luaconf.h"
+
+
+static int flexiprint(lua_State *L)
+{
+    
+    int n = lua_gettop(L);  /* number of arguments */
+    int i;
+    lua_getglobal(L, "tostring");
+    for (i=1; i<=n; i++) {
+        const char *s;
+        size_t l;
+        lua_pushvalue(L, -1);  /* function to be called */
+        lua_pushvalue(L, i);   /* value to print */
+        lua_call(L, 1, 1);
+        s = lua_tolstring(L, -1, &l);  /* get result */
+        if (s == NULL)
+            return luaL_error(L,
+                              LUA_QL("tostring") " must return a string to " LUA_QL("print"));
+        if (i>1) luai_writestring("\t", 1);
+        luai_writestring(s, l);
+        lua_pop(L, 1);  /* pop result */
+    }
+    luai_writestring();
+    return 0;
+
+}
+
+int open_flexiprint(lua_State *L)
+{
+    lua_pushglobaltable(L);
+    return 1;
+}
---
+++ LuaInterp/bdlib.h
@@ -0,0 +1,21 @@
+//
+//  flexiprint.h
+//  LuaInterp
+//
+//  Created by Matthew Burke on 10/26/14.
+//  Copyright (c) 2014 BlueDino Software. All rights reserved.
+//
+
+#ifndef __LuaInterp__flexiprint__
+#define __LuaInterp__flexiprint__
+
+
+#include <stdio.h>
+#include "lua.h"
+#include "lauxlib.h"
+#include "luaconf.h"
+
+int open_flexiprint(lua_State *);
+
+
+#endif /* defined(__LuaInterp__flexiprint__) */
